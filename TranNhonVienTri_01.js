113f4262595607ffeef5d7e635d5c1616394a56b
tri.tran97 <tri.tran24797@gmail.com>
2022-05-09 17:11:20 +0700

    bai 1

---
+++ TranNhonVienTri_01.js
@@ -0,0 +1,62 @@
+//bai1
+let a ="JavaScript - ";
+let b ="\"I love you\" ";
+console.log(a+b);
+
+//bai2
+let a2 = 6;
+let b2 = 3;
+console.log(a2/b2);
+
+//bai3
+let a3 = 5;
+let b3 = 2;
+console.log(a3+b3);
+
+//bai4;
+let doF = 32;
+let doC =((5/9)*(doF-32));
+console.log(doC);
+
+//bai5
+let a5 = 2;
+let b5 = 4;
+console.log((a5**2)+(b5**2));
+
+//bai6
+let a6 = 2;
+let b6 = 4;
+console.log(Math.pow(a6+b6,2));
+
+//bai7
+let a7 = 2;
+let b7 = 4;
+let c7 = 16;
+let mauSo = ((a7**2)*(b7**2));
+console.log(mauSo/c7);
+
+//bai8
+let toan = 1;
+let ly = 2;
+let sinh = 3;
+let anh = 4;
+console.log((toan+anh+ly+sinh)/4);
+
+//bai9
+let a9 = 3;
+let b9 = 0;
+console.log(a9/b9);
+
+//bai10
+let a10 = 3;
+let b10 = -1;
+let c10 = -2;
+let mauSo10 = (-b10+Math.sqrt((b10**2)-(4*a10*c10)));
+console.log(mauSo10/(2*a10));
+
+//bai11
+let x11 = 4 ;
+let y11 = 2;
+let mauSo11 = (Math.sqrt(x11)+Math.abs(x11));
+let tuSo11 =(Math.sqrt(Math.pow(x11,y11)));
+console.log(mauSo11/tuSo11);
